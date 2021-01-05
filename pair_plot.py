99127e362edca65f1c3bba5961f2d2a93967e195
artainmo <tainmontarthur@icloud.com>
2021-01-05 19:30:56 +0100

    final

--- pair_plot.py
+++ pair_plot.py
@@ -15,15 +15,13 @@ if __name__ == "__main__":
 
 #Scatterplots are useful to find correlations and homogenousity between two features.
 #If one of two features are the same, one of them is not interesting for AI and can be eliminated.
-#If features are correlated meaning if value one feature goes into one direction, value of second feature goes into same direction -> forms a straight line with 45degree angle
 
 #Line plots are useful to find correlations between classes in one feature
 #Features that are homogenous or have low variation over the classes are not interesting for AI neither as they have low predictive power
 
 #From this visualization, what features are you going to use for your logistic regression?
 
-#Features: "Arithmancy and care of magic creatures" are homogenous across the classes and can be eliminated
-#"Astronomy and Defense against the dark arts are completely positively correlated", this means we can eliminate one of them.
+#Features: "Arithmancy and care of magic creatures" are homogenous across the classes and at least one can be eliminated
 
-#->Arithmancy, care of magic creatures and Astronomy are the final eliminated features
+#->Arithmancy is the final eliminated feature
 #Eliminating features speeds up the algorithm
